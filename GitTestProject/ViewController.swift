6111c693a465979e8ec85624dd00fc14c45bf9cb
serdar <sserdar@isbank.net.tr>
2021-01-29 15:06:43 +0300

    yorum sildim

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Serdar on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
